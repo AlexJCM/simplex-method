dda574f4933fa51ee337a8f71c4424c368fbcb73
AlexK <alexjhon@live.com>
2019-12-19 22:34:38 -0500

    Initial commit

--- src/components/HeaderEcuaciones.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const HeaderEcuaciones = () => (
-  <header className="Home-header">
-    <p>Ecuaciones Metodo Simplex</p>
-  </header>
-);
-
-export default HeaderEcuaciones;
